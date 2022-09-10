597d7fa3a80a54429efd035dd10b28d81d2a5588
SHIVAMCHARDE <83965173+SHIVAMCHARDE@users.noreply.github.com>
2022-09-10 23:27:19 +0530

    NavBar_Added

--- artisticdots/src/Pages/Home/Home.jsx
+++ artisticdots/src/Pages/Home/Home.jsx
@@ -1,13 +1,32 @@
 import React from 'react'
+
 import './Home.css'
 
 export default function Home() {
   return (
     <div className='HomeContainer'>
 
-      <div className='HomeContent'>
+      <section className='HomeContent'>
         <p>   "For Those with ART as their passion" </p>
-      </div>
+      </section>
+
+      <section className='scroll'>scroll</section>
+
+
+<section className='Hall_Of_Art'>
+
+<section className='Hall_Of_Art_Header'>HALL OF ART</section>
+<section className='Hall_Of_Art_img'>
+
+
+
+</section>
+
+
+
+
+
+</section>
 
 
     </div>
