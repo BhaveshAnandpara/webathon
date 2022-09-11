af356ebb19b5417366ea7983cf107b07e47d845b
Bhavesh Anandpara <bhaveshAP09112002@gmail.com>
2022-09-11 11:41:07 +0530

    APIs and Database Done

--- artisticdots/src/Pages/Home/Home.jsx
+++ artisticdots/src/Pages/Home/Home.jsx
@@ -1,84 +1,82 @@
 import React from 'react'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls' //(use it after camera)
-import { BufferAttribute, BufferGeometry } from 'three'
+import { BufferAttribute, BufferGeometry, cloneUniformsGroups } from 'three'
 // import dat from 'dat.gui'
 import { RectAreaLightHelper } from 'three/examples/jsm/helpers/RectAreaLightHelper.js'
 import canvas from '../../Pages/Home/Home'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import texture from '../../Assets/textures/1.png'
+
 
 
 export default function Home() {
 
-  useEffect(() => {
+  const [imgArr, setImgArr] = useState([])
 
-    const scene = new THREE.Scene()
+  // useEffect(() => {
 
-    for (let i = -10; i < 5; i++) {
+  //   const scene = new THREE.Scene()
 
-
-      const geometry = new THREE.CircleGeometry(1, 32)
-
-      const material = new THREE.MeshBasicMaterial({
-        color: 0xE0BBE4, //  color : 'red' , color : '#ff0000' ,  also works
-        // wireframe : true //Shows Wiref,rame 
-      });
-
-      const mesh = new THREE.Mesh(geometry, material)
-      mesh.position.set(0.3 * i , 1,)
-      mesh.scale.set(0.1, 0.1, 0.1)
-
-      scene.add(mesh)
-    }
-
-    const sizes = {
-
-      width: 800,
-      height: 600,
-    }
+  //   for (let i = -10; i < 5; i++) {
 
 
-    const camera = new THREE.PerspectiveCamera(70, (sizes.width / sizes.height), 1, 1000)
+  //     const geometry = new THREE.CircleGeometry(1, 32)
+
+  //     const material = new THREE.MeshBasicMaterial({
+  //       color: 0xE0BBE4, //  color : 'red' , color : '#ff0000' ,  also works
+  //       // wireframe : true //Shows Wiref,rame 
+  //     });
+
+  //     const mesh = new THREE.Mesh(geometry, material)
+  //     mesh.position.set(0.3 * i , 1,)
+  //     mesh.scale.set(0.1, 0.1, 0.1)
+
+  //     scene.add(mesh)
+  //   }
+
+  //   const sizes = {
+
+  //     width: 800,
+  //     height: 600,
+  //   }
 
 
-    camera.position.z = 3// used 3 beacuse cube is of 1 unit so got away from cube by 3 units 
-    // camera.lookAt(mesh.position)
-    scene.add(camera)
-
-    const canvas = document.querySelector('.webgl')
-
-    const renderer = new THREE.WebGLRenderer({
-      canvas: canvas,  // Canvas for WEBGL
-    })
-
-    renderer.setSize(sizes.width, sizes.height)
-
-    const tick = () => { //Creating a function for Animation 
+  //   const camera = new THREE.PerspectiveCamera(70, (sizes.width / sizes.height), 1, 1000)
 
 
-      //Update Controls
+  //   camera.position.z = 3// used 3 beacuse cube is of 1 unit so got away from cube by 3 units 
+  //   // camera.lookAt(mesh.position)
+  //   scene.add(camera)
 
-      //render
-      renderer.render(scene, camera);
+  //   const canvas = document.querySelector('.webgl')
 
-      window.requestAnimationFrame(tick) // requestAnimationFrame() runs at 60 Frames per Second(FPS)
+  //   const renderer = new THREE.WebGLRenderer({
+  //     canvas: canvas,  // Canvas for WEBGL
+  //   })
+
+  //   renderer.setSize(sizes.width, sizes.height)
+
+  //   const tick = () => { //Creating a function for Animation 
 
 
-    }
+  //     //Update Controls
 
-    tick()
+  //     //render
+  //     renderer.render(scene, camera);
 
-  }, [])
+  //     window.requestAnimationFrame(tick) // requestAnimationFrame() runs at 60 Frames per Second(FPS)
+
+
+  //   }
+
+  //   tick()
+
+  // }, [])
+
 
 
   return (
-<<<<<<< HEAD
-    <>
-      <div>Home</div>
-      <canvas className="webgl"></canvas>
-    </>
-=======
     <div className='HomeContainer'>
 
       <section className='HomeContent'>
@@ -88,22 +86,20 @@ export default function Home() {
       <section className='scroll'>scroll</section>
 
 
-<section className='Hall_Of_Art'>
+      <section className='Hall_Of_Art'>
 
-<section className='Hall_Of_Art_Header'>HALL OF ART</section>
-<section className='Hall_Of_Art_img'>
+        <section className='Hall_Of_Art_Header'>HALL OF ART</section>
 
-{/* ya pr function banky de */}
-</section>
+        <div className='Hall_Of_Art_img'>
 
 
 
+        </div>
 
-</section>
+      </section>
 
 
     </div>
->>>>>>> b4d190ca2f51ac3a03247cad5e60ec9907c85ec7
   )
 }
 
