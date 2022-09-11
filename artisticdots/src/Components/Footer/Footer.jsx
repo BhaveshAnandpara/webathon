4110ba82aebeb03dd2a3fed03310d5c8363bde50
vaishna2002 <co.2020.vabhende@bitwardha.ac.in>
2022-09-11 14:01:05 +0530

    hall of art structure

---
+++ artisticdots/src/Components/Footer/Footer.jsx
@@ -0,0 +1,19 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className='info'>
+        <div className='contactus'>
+          
+
+        </div>
+        <div className='link'>
+
+        </div>
+          
+      </div>
+  )
+}
+
+const styles = StyleSheet.create({})
